713d968c7498c36b647e52eb8e1fd1f9b8c844fb
Jesus <jdgonzalez815@misena.edu.co>
2022-10-15 11:56:22 -0500

    otro commitss

--- registrar.php
+++ registrar.php
@@ -2,7 +2,7 @@
   <?php
   require 'conexion.php';
   
-  mysqli_query($conexion, "INSERT INTO `banco`(`nombre`, `apellido`, `tipoident`, `numid`, `telefono`, `direccion`, `email`) VALUES ('[nombre]','[apellido]','[tipoident]','[numid]','[telefono]','[direccion]','[email]')")
+  mysqli_query($conexion, "INSERT INTO `banco`(`nombre`, `apellido`, `tipoident`, `numid`, `telefono`, `direccion`, `email`) VALUES ('$_REQUEST[nombre]','$_REQUEST[apellido]','$_REQUEST[tipoident]',$_REQUEST[numid],$_REQUEST[telefono],'$_REQUEST[direccion]','$_REQUEST[email]')")
       mysqli_close($conexion);
     
 header("location: datos.php") ;
