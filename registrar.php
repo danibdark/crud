1c90b69b2a46a1d0f11f099bc7468eb386a56990
Jesus <jdgonzalez815@misena.edu.co>
2022-10-15 11:45:01 -0500

    5 comit

--- registrar.php
+++ registrar.php
@@ -2,11 +2,11 @@
   <?php
   require 'conexion.php';
   
-  
   mysqli_query($conexion, "insert into banco(nombre,apellido, tipoident, numid, telefono, direccion, email) values 
         ('$_REQUEST[nombre]','$_REQUEST[apellido]','$_REQUEST[tipoident]',$_REQUEST[numid],$_REQUEST[telefono],'$_REQUEST[direccion]','$_REQUEST[email]')")
         or die("Problemas en el select" . mysqli_error($conexion));
+
+        header("location: datos.php") ;
       mysqli_close($conexion);
     
-header("location: datos.php") ;
 ?>
